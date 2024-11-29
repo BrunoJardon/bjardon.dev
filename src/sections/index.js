0d72bfa28875996fea5bdaf0dc3be20e2ef3c115
Bruno Jardon <brunojardon3@gmail.com>
2024-11-29 14:48:51 -0300

    index sections

---
+++ src/sections/index.js
@@ -0,0 +1,7 @@
+export { default as AboutMe } from "./AboutMe.astro";
+export { default as Experiences } from "./Experiences.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as Hero } from "./Hero.astro";
+export { default as Projects } from "./Projects.astro";
+export { default as Skills } from "./Skills.astro";
+export { default as Studies } from "./Studies.astro";
