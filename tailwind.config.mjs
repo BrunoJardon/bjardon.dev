99119a209bb8ea95ee8a706088e731f1aae5a561
Bruno Jardon <brunojardon3@gmail.com>
2024-12-02 20:35:46 -0300

    colors added to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'gradient-a': 'var(--gradient-color-a)',
+				'gradient-b': 'var(--gradient-color-b)',
+				
+				'font-a': 'var(--font-color-a)',
+				'font-b': 'var(--font-color-b)',
+
+				'background-a': 'var(--bg-color-a)',
+				'background-b': 'var(--bg-color-b)',
+			}
+		},
 	},
 	plugins: [],
 }
